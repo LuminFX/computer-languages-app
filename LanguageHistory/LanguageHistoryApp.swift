cb24adf84eb538d5891ced39af0e7aa6de516ce5
LuminMCB <tmschmitz@cox.net>
2023-04-12 10:29:34 -0600

    Initial Commit

---
+++ LanguageHistory/LanguageHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageHistoryApp.swift
+//  LanguageHistory
+//
+//  Created by Tyler Schmitz on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LanguageHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
